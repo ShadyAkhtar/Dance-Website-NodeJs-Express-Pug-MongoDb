c6110bd024d6258c823417bf2d2625d92edb21ef
Shadab <akhtarshadab415@gmail.com>
2021-01-08 21:53:32 +0530

    Integrated Express with  NodeJS and designed using Pug and CSS

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+// app.get('/', (req, res)=>{
+//     res.status(200).render('index.pug'); // changed to home.pug after implementing template inheritance
+// })
+app.get('/', (req, res)=>{
+    res.status(200).render('home.pug');
+})
+app.get('/contact', (req, res)=>{
+    res.status(200).render('contact.pug');
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
